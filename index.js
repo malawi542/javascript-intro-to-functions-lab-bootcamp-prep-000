a152e2e63aa57d5bca2b9490ef7c7c366bc0f58e
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-06-08 05:44:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+var uppercase = "HELLO!";
+
+uppercase.toUpperCase() === uppercase;
+
+var lowerCase = 'hello!';
+
+lowerCase === lowerCase.toLowerCase();
+
+var mixedCase = 'Hi there!';
+
+mixedCase.toLowerCase() === mixedCase;
+
+mixedCase.toUpperCase() === mixedCase;
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you!";
+  } else if (HELLO === uppercase) {
+    return "YES INDEED!"
+  }
+}
